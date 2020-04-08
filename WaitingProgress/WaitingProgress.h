e6807ceb710b4c843c4dd2ed220fedcb34d1d223
Hung Cao <cthung.it2013@gmail.com>
2020-04-08 08:09:28 +0700

    creating a progress in line

---
+++ WaitingProgress/WaitingProgress.h
@@ -0,0 +1,19 @@
+//
+//  WaitingProgress.h
+//  WaitingProgress
+//
+//  Created by Hung Cao on 4/8/20.
+//  Copyright © 2020 Hung Cao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaitingProgress.
+FOUNDATION_EXPORT double WaitingProgressVersionNumber;
+
+//! Project version string for WaitingProgress.
+FOUNDATION_EXPORT const unsigned char WaitingProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaitingProgress/PublicHeader.h>
+
+
